452c0978ae75c7d1e03ac3f98e9fb0fd2933cd33
m1sh3r <mish.fe2004@gmail.com>
2024-06-16 19:54:31 +0300

    fix theme toggler

--- js/toggletheme.js
+++ js/toggletheme.js
@@ -1,12 +1,16 @@
+let animationsDisabled = false;
+
 function toggleTheme() {
   var body = document.body;
-  var isDarkMode = body.classList.contains("dark-mode");
+  var isDarkMode = body.classList.contains("dark");
 
   if (isDarkMode) {
-    body.classList.remove("dark-mode");
+    body.classList.remove("dark");
+    body.classList.add("light");
     localStorage.setItem("theme", "light");
   } else {
-    body.classList.add("dark-mode");
+    body.classList.remove("light");
+    body.classList.add("dark");
     localStorage.setItem("theme", "dark");
   }
 }
@@ -14,12 +18,56 @@ function toggleTheme() {
 function setThemeFromLocalStorage() {
   var theme = localStorage.getItem("theme");
   if (theme === "dark") {
-    document.body.classList.add("dark-mode");
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
   } else {
-    document.body.classList.remove("dark-mode");
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
   }
+
+  if (theme === "dark" && !animationsDisabled) {
+    temporaryDisableAnimations();
+  }
+}
+
+function temporaryDisableAnimations() {
+  document.documentElement.classList.add("disable-animation");
+  animationsDisabled = true;
+
+  setTimeout(function () {
+    document.documentElement.classList.remove("disable-animation");
+
+    document.querySelectorAll(".sidemenu").forEach(function (element) {
+      element.style.transition = "";
+      element.style.visibility = "";
+    });
+  }, 200);
+}
+
+function saveTheme(theme) {
+  localStorage.setItem("theme", theme);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   setThemeFromLocalStorage();
+
+  var darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  darkModeMediaQuery.addListener(handleThemeChange);
+  handleThemeChange(darkModeMediaQuery);
 });
+
+function handleThemeChange(event) {
+  if (event.matches) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    saveTheme("dark");
+
+    if (!animationsDisabled) {
+      temporaryDisableAnimations();
+    }
+  } else {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    saveTheme("light");
+  }
+}
