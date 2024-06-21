ccb4aed0aaf8d4559b7136aaf769d011ac076871
m1sh3r <mish.fe2004@gmail.com>
2024-06-21 13:52:12 +0300

    delete z-index set

--- js/photo_zoom.js
+++ js/photo_zoom.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
         this.style.filter = "drop-shadow(30px 10px 4px transparent)";
       } else {
         this.style.transform = "scale(1.5)";
-        this.style.zIndex = "1000";
         this.style.filter = "drop-shadow(0px 0px 40px #00000020)";
       }
     });
