37c22ffe7500fe5c7c191b43fe6a9b0bcffff6e7
мисхер <mish.fe2004@gmail.com>
2024-06-27 17:17:13 +0300

    minor reworking

--- js/photo_zoom.js
+++ js/photo_zoom.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   images.forEach((image) => {
     image.addEventListener("click", function () {
-      if (this.style.transform === "scale(1.5)") {
+      if (this.style.transform.includes("scale(1.5)")) {
         this.style.transform = "scale(1)";
         this.style.filter = "drop-shadow(0px 0px 0px transparent)";
+        this.style.cursor = "zoom-in";
       } else {
         this.style.transform = "scale(1.5)";
         this.style.filter = "drop-shadow(0px 0px 20px var(--for-shadow))";
+        this.style.cursor = "zoom-out";
       }
     });
   });
