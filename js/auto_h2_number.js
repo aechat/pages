c53781345e0e47e31ff5ec3f4e696aca2289051a
мисхер <mish.fe2004@gmail.com>
2024-07-20 19:05:13 +0300

    add numbers to summary and ugly style for this

--- js/auto_h2_number.js
+++ js/auto_h2_number.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const containers = document.querySelectorAll(".content");
   containers.forEach((container, containerIndex) => {
-    const h2s = container.querySelectorAll("h2");
-    const summaries = container.querySelectorAll("details.content_spoiler");
-    [...h2s, ...summaries].forEach((element, index) => {
-      element.textContent = `${containerIndex + 1}.${index + 1}. ${element.textContent}`;
-      element.id = `${containerIndex + 1}.${index + 1}`;
-      if (
-        element.tagName === "DETAILS" &&
-        element.classList.contains("content_spoiler")
-      ) {
-        element.id += `${containerIndex + 1}.${index + 1}`;
+    const h2s = container.querySelectorAll("summary");
+    [...h2s].forEach((element, index) => {
+      if (element.tagName === "SUMMARY" && !element.parentElement.closest("details > summary")) {
+        element.id = `${containerIndex + 1}.${index + 1}`;
       }
+      element.textContent = `${containerIndex + 1}.${index + 1}. ${element.textContent}`;
     });
   });
 });
+
